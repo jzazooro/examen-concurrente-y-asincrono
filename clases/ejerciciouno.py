fa3f184da24f149abfb954d4586454c3dd134761
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-04-24 18:31:22 +0200

    arreglo errores

--- clases/ejerciciouno.py
+++ clases/ejerciciouno.py
@@ -21,22 +21,20 @@ cuenta = CuentaBancaria(100)
 
 for i in range(40):
     cuenta.depositar(100)
-    print("Se ha realizado un deposito en su cuenta de 100 euros, su saldo actual es de: ", cuenta.saldo)
 for i in range(20):
     cuenta.depositar(50)
-    print("Se ha realizado un deposito en su cuenta de 50 euros, su saldo actual es de: ", cuenta.saldo)
+    
 for i in range(60):
     cuenta.depositar(20)
-    print("Se ha realizado un deposito en su cuenta de 20 euros, su saldo actual es de: ", cuenta.saldo)
+    
 
 for i in range(40):
     cuenta.retirar(100)
-    print("Se ha realizado un retiro en su cuenta de 100 euros, su saldo actual es de: ", cuenta.saldo)
+    
 for i in range(20):
     cuenta.retirar(50)
-    print("Se ha realizado un retiro en su cuenta de 50 euros, su saldo actual es de: ", cuenta.saldo)
+    
 for i in range(60):
     cuenta.retirar(20)
-    print("Se ha realizado un retiro en su cuenta de 20 euros, su saldo actual es de: ", cuenta.saldo)
+    
 
-print("Ya se han realizado todas las operaciones, su saldo actual es de: ", cuenta.saldo)
